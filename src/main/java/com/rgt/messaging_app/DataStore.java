c213debbf282b9f3122202eca02293e0abee3d0f
AshokPasula <ashok.pasula@ratnaglobaltech.com>
2023-06-25 11:52:36 +0530

    Added RGT message app

---
+++ src/main/java/com/rgt/messaging_app/DataStore.java
@@ -0,0 +1,27 @@
+package com.rgt.messaging_app;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class DataStore {
+	private Map<String, User> users;
+
+	public DataStore() {
+		this.users = new HashMap<String, User>();
+	}
+
+	public void saveData(String userName, User newUser) {
+		users.put(userName, newUser);
+	}
+
+	public Map<String, User> loadData() {
+		return users;
+	}
+
+	public int generateTweetId() {
+		Random random = new Random();
+		int tweetId = random.nextInt(1000);
+		return tweetId;
+	}
+}
